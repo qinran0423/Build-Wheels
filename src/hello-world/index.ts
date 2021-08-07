e818dbd2d885429039413631897e01da942d22da
qinran0423 <1156107187@qq.com>
2021-08-07 17:07:56 +0800

    ts

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -2,7 +2,7 @@
 // 2.返回一个函数，函数接受一个函数参数，如果传递该参数则执行，同时传入hi给他
 // 3.返回helloworld
 
-module.exports = function(fn) {
+module.exports = function(fn: any):string{
   fn && fn('hi');
-  return 'helloworld'
+  return 'helloworld';
 }
